26ad086a69b66e5391bfec10be133eac3c701b72
Xinshuai Qi <qxs718@gmail.com>
2018-10-29 11:07:00 -0500

    Create R_txt_parse.R

---
+++ R_txt_parse.R
@@ -0,0 +1,20 @@
+###
+process txt files with multiple header lines
+
+
+
+ref https://stackoverflow.com/questions/32615039/how-to-parse-text-file-with-multiple-sections-of-headers-in-r
+
+setwd("/efs/home/qi_evrpa/canola65037/TRF/")
+setwd("C:/Users/evrpa/Desktop/TRF")
+f<- readLines("XXX.dat")
+head(f)
+headers = which(grepl("^Sequence:", f))
+headers
+
+ff = split(f, cumsum(seq_along(f) %in% headers))
+class(ff)
+l.in = lapply(ff, function(x) read.table(text=x, header=T, sep="|", fill=T, stringsAsFactors=F))
+
+names(l.in) = sapply(l.in, function(x) names(x)[1])
+
